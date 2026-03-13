4bd4e4a5efc77ece875ba5900e5b0715c16138a3
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-03-13 22:30:40 +0530

    Add initial implementation of file sorting module

---
+++ sorter.js
@@ -0,0 +1,20 @@
+import fs from "fs/promises";
+import path from "path";
+
+
+export default async function SortFiles(SourceFolder, DestinationFolder) {
+  let ext, foldername;
+
+  let bfFiles = await fs.readdir(`./${SourceFolder}`);
+
+  for (let i = 0; i < bfFiles.length; i++) {
+    ext = path.extname(bfFiles[i]);
+    foldername = ext.slice(1);
+
+    await fs.mkdir(`./${DestinationFolder}/${foldername}`, { recursive: true });
+    await fs.rename(
+      path.resolve(`./${SourceFolder}/${bfFiles[i]}`),
+      `./${DestinationFolder}/${foldername}/${bfFiles[i]}`,
+    );
+  }
+}
